e2de8a8d8b5edab93062cb47110a79ac890d4449
amir <redwolf_ir@outlook.com>
2020-07-12 21:47:02 +0430

    some changes

--- games/models.py
+++ games/models.py
@@ -4,15 +4,42 @@ from django.dispatch import receiver
 from django.db import models
 
 def game_cover_upload_path(instance, filename):
-    if " " in instance.album.title:
-        instance.name.title.replace(" ", "_")
+    if " " in instance.GAME.title:
+        instance.title.title.replace(" ", "_")
     return '/'.join([instance.name.title, filename])
 
+def d_logo_upload_path(instance, filename):
+    if " " in instance.GAME.title:
+        instance.title.title.replace(" ", "_")
+    return '/'.join([instance.name.title, filename])
+
+class DEVELOPER(models.Model):
+    title = models.CharField(max_length = 50, varbose_name = "توسعه دهنده")
+    about = models.TextField()
+    logo =  models.FileField(upload_to = d_logo_upload_path,
+    keep_orphans = True, verbose_name = 'لوگو')
+    slug = models.SlugField(unique=True, max_length = 100)
+
+class PUBLISHER(models.Model):
+    title = models.CharField(max_length = 50, varbose_name = "ناشر")
+    about = models.TextField()
+    logo =  models.FileField(upload_to = d_logo_upload_path,
+    keep_orphans = True, verbose_name = 'لوگو')
+    slug = models.SlugField(unique=True, max_length = 100)
+
+class GENRE(models.Model):
+    title = models.CharField(max_length = 50, varbose_name = "عنوان ژانر")
+    about = models.TextField()
+    slug = models.SlugField(unique=True, max_length = 100)
+
+    def __unicode__(self):
+        return self.title
+
 class GAME(models.Model):
-    name = models.CharField(max_length = 50, verbose_name = "عنوان بازی")
+    title = models.CharField(max_length = 50, verbose_name = "عنوان بازی")
     release_date = models.DateTimeField(null = True, blank = True,
     verbose_name="تاریخ انتشار")
-    cover = models.FileField(upload_to = upload_path, keep_orphans = True,
+    cover = models.FileField(upload_to = game_cover_upload_path, keep_orphans = True,
     verbose_name = 'کاور بازی')
     slug = models.SlugField(unique=True, max_length = 100)
     genre = models.ManyToManyField(GENRE, verbose_name = "سبک بازی")
@@ -29,3 +56,6 @@ class GAME(models.Model):
     ]
     Age_rating = models.CharField(max_length = 2,
     choices = RATINGESRB, default = RATINGPENDING)
+
+    def __unicode__(self):
+        return self.title
